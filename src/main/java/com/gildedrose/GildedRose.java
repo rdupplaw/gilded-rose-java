55ac08e60f71e88b0893df8b326c259c385fdd74
Robert Dupplaw <dupplawrobert@gmail.com>
2022-12-15 00:03:15 +0000

    Replace conditionals with polymorphism

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,127 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class GildedRose {
     Item[] items;
+    private final List<InventoryItem> inventoryItems;
 
     public GildedRose(Item[] items) {
         this.items = items;
+        this.inventoryItems = Arrays.stream(items).map(this::mapItemToInventoryItem).collect(Collectors.toList());
+    }
+
+    InventoryItem mapItemToInventoryItem(Item item) {
+        switch (item.name) {
+            case "Aged Brie":
+                return new AgedBrie(item);
+            case "Backstage passes to a TAFKAL80ETC concert":
+                return new BackstagePass(item);
+            case "Sulfuras, Hand of Ragnaros":
+                return new Sulfuras();
+            default:
+                return new GenericInventoryItem(item);
+        }
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+        for (InventoryItem item : inventoryItems) {
+            item.updateQuality();
+        }
+    }
+
+    private interface InventoryItem {
+        void updateQuality();
+    }
+
+    private static class GenericInventoryItem implements InventoryItem {
+        private final Item item;
+
+        public GenericInventoryItem(Item item) {
+            this.item = item;
+        }
+
+        @Override
+        public void updateQuality() {
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
+
+            item.sellIn = item.sellIn - 1;
+
+            if (item.sellIn < 0) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+            }
+        }
+    }
+
+    private static class AgedBrie implements InventoryItem {
+        private final Item item;
+
+        public AgedBrie(Item item) {
+            this.item = item;
+        }
+
+        @Override
+        public void updateQuality() {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+
+            item.sellIn = item.sellIn - 1;
+
+            if (item.sellIn < 0) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
+    }
+
+    private static class BackstagePass implements InventoryItem {
+        private final Item item;
+
+        public BackstagePass(Item item) {
+            this.item = item;
+        }
+
+        @Override
+        public void updateQuality() {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            item.sellIn = item.sellIn - 1;
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+            if (item.sellIn < 0) {
+                item.quality = 0;
             }
+        }
+    }
+
+    private static class Sulfuras implements InventoryItem {
+
+        public Sulfuras() {
+        }
+
+        @Override
+        public void updateQuality() {
         }
     }
 }
